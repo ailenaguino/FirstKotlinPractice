dc6fd62f829e90c3dbb737cb98100a970853176e
DESKTOP-6LDNJGR\54113 <ailenaguino@gmail.com>
2024-02-02 18:37:08 -0300

    Starting TODO App, new theme added

--- Practica1/app/src/main/java/com/ailenaguino/practica1/MenuActivity.kt
+++ Practica1/app/src/main/java/com/ailenaguino/practica1/MenuActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.ailenaguino.practica1.firstApp.FirstAppActivity
 import com.ailenaguino.practica1.imcCalculator.ImcCalculatorActivity
+import com.ailenaguino.practica1.toDoApp.ToDoAppActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +14,15 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnMsgApp = findViewById<Button>(R.id.btnMsgApp)
         val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnToDoApp = findViewById<Button>(R.id.btnToDoApp)
         btnMsgApp.setOnClickListener { navigateToMsgApp() }
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnToDoApp.setOnClickListener { navigateToToDoApp() }
+    }
+
+    private fun navigateToToDoApp() {
+        val intent = Intent(this, ToDoAppActivity::class.java)
+        startActivity(intent)
     }
 
     private fun navigateToIMCApp() {
---
+++ Practica1/app/src/main/java/com/ailenaguino/practica1/toDoApp/ToDoAppActivity.kt
@@ -0,0 +1,12 @@
+package com.ailenaguino.practica1.toDoApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ailenaguino.practica1.R
+
+class ToDoAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do_app)
+    }
+}
