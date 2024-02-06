a93ef1c1636f72f7e3d986b6b917759fe50d4033
DESKTOP-6LDNJGR\54113 <ailenaguino@gmail.com>
2024-02-05 23:30:47 -0300

    Done = Dialog

--- Practica1/app/src/main/java/com/ailenaguino/practica1/toDoApp/ToDoAppActivity.kt
+++ Practica1/app/src/main/java/com/ailenaguino/practica1/toDoApp/ToDoAppActivity.kt
@@ -3,6 +3,11 @@ package com.ailenaguino.practica1.toDoApp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ailenaguino.practica1.R
@@ -46,6 +51,30 @@ class ToDoAppActivity : AppCompatActivity() {
     private fun showDialog() {
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_task)
+//        val lp = dialog.window?.attributes;
+//        if (lp != null) {
+//            lp.dimAmount = 0.7f
+//        }
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            if (etTask.text.toString().isNotEmpty()) {
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val currentCategory: TaskCategory = when (selectedRadioButton.text) {
+                    getString(R.string.business) -> Business
+                    getString(R.string.personal) -> Personal
+                    else -> Other
+                }
+
+                tasks.add(Task(etTask.text.toString(), currentCategory))
+                updateTasks()
+                dialog.hide()
+            }
+        }
         dialog.show()
     }
 
@@ -54,7 +83,7 @@ class ToDoAppActivity : AppCompatActivity() {
         rvCategory.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategory.adapter = categoryAdapter
 
-        tasksAdapter = TasksAdapter(tasks)
+        tasksAdapter = TasksAdapter(tasks) {position -> onItemSelected(position)}
         rvTasks.layoutManager = LinearLayoutManager(this) //no se pone lo demas porque es vertical
         rvTasks.adapter = tasksAdapter
     }
@@ -63,5 +92,13 @@ class ToDoAppActivity : AppCompatActivity() {
         rvCategory = findViewById(R.id.rvCategory)
         rvTasks = findViewById(R.id.rvTasks)
         fabAddTask = findViewById(R.id.fabAddTask)
+    }
+
+    private fun onItemSelected(position:Int){
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+    private fun updateTasks() {
+        tasksAdapter.notifyDataSetChanged()
     }
 }
