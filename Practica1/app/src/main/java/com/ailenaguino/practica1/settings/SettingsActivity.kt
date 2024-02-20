235c860595d2066d3ea076a41375cf0a4fe3b348
DESKTOP-6LDNJGR\54113 <ailenaguino@gmail.com>
2024-02-20 15:47:03 -0300

    Settings is finished

--- Practica1/app/src/main/java/com/ailenaguino/practica1/settings/SettingsActivity.kt
+++ Practica1/app/src/main/java/com/ailenaguino/practica1/settings/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.ailenaguino.practica1.settings
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
@@ -12,6 +13,11 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.ailenaguino.practica1.databinding.ActivitySettingsBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settingsDB")
@@ -26,13 +32,27 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivitySettingsBinding
+    private var firstTime = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySettingsBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            getSettings().filter { firstTime }.collect { settingsModel -> //collect : me engancho al flow y cada vez que haya un cambio avisame
+                if (settingsModel != null) {
+                    runOnUiThread {
+                        binding.swVibration.isChecked = settingsModel.vibration
+                        binding.swBluetooth.isChecked = settingsModel.bluetooth
+                        binding.swDarkMode.isChecked = settingsModel.darkmode
+                        binding.rsVolume.setValues(settingsModel.volume.toFloat())
+                        firstTime = !firstTime
+                    }
+                }
+            }
+        }
+        initUI()
     }
 
     private fun initUI() {
@@ -42,19 +62,22 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
-        binding.swDarkMode.setOnCheckedChangeListener{_ , bool ->
+        binding.swDarkMode.setOnCheckedChangeListener { _, bool ->
+
+            if(bool) enableDarkMode() else disableDarkMode()
+
             CoroutineScope(Dispatchers.IO).launch {
                 saveOptions(bool, DARK_MODE)
             }
         }
 
-        binding.swBluetooth.setOnCheckedChangeListener{_ , bool ->
+        binding.swBluetooth.setOnCheckedChangeListener { _, bool ->
             CoroutineScope(Dispatchers.IO).launch {
                 saveOptions(bool, BLUETOOTH)
             }
         }
 
-        binding.swVibration.setOnCheckedChangeListener{_ , bool ->
+        binding.swVibration.setOnCheckedChangeListener { _, bool ->
             CoroutineScope(Dispatchers.IO).launch {
                 saveOptions(bool, VIBRATION)
             }
@@ -69,6 +92,28 @@ class SettingsActivity : AppCompatActivity() {
 
     private suspend fun saveOptions(value: Boolean, key: String) {
         dataStore.edit { preference ->
-            preference[booleanPreferencesKey(key)] = value }
+            preference[booleanPreferencesKey(key)] = value
+        }
+    }
+
+    private fun getSettings(): Flow<SettingsModel> {
+        return dataStore.data.map {
+            SettingsModel(
+                volume = it[intPreferencesKey(VOLUME_LVL)] ?: 50,
+                darkmode = it[booleanPreferencesKey(DARK_MODE)] ?: false,
+                bluetooth = it[booleanPreferencesKey(BLUETOOTH)] ?: true,
+                vibration = it[booleanPreferencesKey(VIBRATION)] ?: true
+            )
+        }
+    }
+
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        delegate.applyDayNight()
     }
 }
---
+++ Practica1/app/src/main/java/com/ailenaguino/practica1/settings/SettingsModel.kt
@@ -0,0 +1,8 @@
+package com.ailenaguino.practica1.settings
+
+data class SettingsModel(
+    var volume: Int,
+    var bluetooth: Boolean,
+    var darkmode: Boolean,
+    var vibration: Boolean
+)
